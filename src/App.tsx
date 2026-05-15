e230b3f2902f8005f27fb8844dcfca60473c50c2
J. Roberto <jrmacbrandt@gmail.com>
2026-05-14 23:02:19 -0300

    style: subir so mais um pouco o botao hero

--- src/App.tsx
+++ src/App.tsx
@@ -115,7 +115,7 @@ export default function App() {
                 </span>
               </div>
 
-              <PremiumButton className="translate-y-[50px]">
+              <PremiumButton className="translate-y-[45px]">
                 GARANTIR MEU PACK
               </PremiumButton>
             </div>
